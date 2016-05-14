7b2e5bf6f51f9f7720d9f42f1364750c781f8352
tprupsys <tprupsys@gmail.com>
2016-05-14 17:20:00 +0300

    Added stream service

--- app/childmode/childmode.component.ts
+++ app/childmode/childmode.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { StreamService } from '../services/stream/stream.service';
 
 
 @Component({
+  providers: [StreamService],
   directives: [ROUTER_DIRECTIVES],
   template: `
     <div class="mode-container">
         <div class="header">Child mode</div>
         <div class="content">
-            Lorem ipsum blabla
+            <button (click)="startStreaming()">Start streaming</button>
         </div>
         <div class="footer"><a [routerLink]="['/modeselection']">Back</a></div>
     </div>
     `
 })
 export class ChildMode {
-
+    constructor(private streamService: StreamService) {}
+    
+    startStreaming() {
+        this.streamService.start();
+    }
 }
---
+++ app/services/stream/stream.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class StreamService {
+
+    webSocket: WebSocket = undefined;
+    currentStream = undefined;
+    ended: Boolean = false;
+    
+    start() {
+       this.ended = false;
+       this.webSocket = new WebSocket('ws://localhost:8080/hello');
+       this.webSocket.binaryType = 'arraybuffer';
+       this.webSocket.onopen = this.initWebSocket;
+    }
+    
+    stop() {
+        this.currentStream.getTracks()[0].stop();
+    }
+    
+    private initWebSocket() {
+        var n = <any> navigator;
+        n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia || n.msGetUserMedia;
+        n.getUserMedia({audio: true}, this.initRecorder, this.errorCallback);
+    }
+    
+    private initRecorder(stream) {
+        this.currentStream = stream;
+        this.currentStream.addEventListener('ended', function() { 
+            this.ended = true;
+        });
+
+        let audioContext = new AudioContext();
+        let sampleRate = audioContext.sampleRate;
+        var audioInput = audioContext.createMediaStreamSource(stream);
+
+        var bufferSize = 16384;
+        var recorder = audioContext.createScriptProcessor(bufferSize, 1, 1);
+        recorder.onaudioprocess = this.recorderProcess;
+        audioInput.connect(recorder);
+        recorder.connect(audioContext.destination);
+    }
+    
+    private recorderProcess(e) {
+        if (this.ended) return;
+        var left = e.inputBuffer.getChannelData(0);
+        var data = left;
+        this.webSocket.send(data);
+    }
+    
+    private errorCallback(error) {
+        console.log(error);
+    }
+}

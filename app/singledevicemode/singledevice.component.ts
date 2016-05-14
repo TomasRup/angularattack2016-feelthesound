e22dace3415de5952c4ed41f4b758fd0f6812b8f
Matas Ramoška <matas.ramoska@tieto.com>
2016-05-14 16:40:04 +0300

    voice service

--- app/singledevicemode/singledevice.component.ts
+++ app/singledevicemode/singledevice.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { VoiceService } from '../services/voice/voice.service';
 
 
 @Component({
+  providers: [VoiceService],
   directives: [ROUTER_DIRECTIVES],
   template: `
     <div class="mode-container">
         <div class="header">Single device mode</div>
         <div class="content">
-            Lorem ipsum blabla
+            <button (click) ="toggleInput()">live input</button>
+            <canvas id="canvas"></canvas>
         </div>
         <div class="footer"><a [routerLink]="['/modeselection']">Back</a></div>
     </div>
     `
 })
 export class SingleDevice {
+       
+    constructor(private voiceService: VoiceService) {
+        
+    }
 
+    toggleInput() {
+        console.log(this.voiceService);
+    }
 }

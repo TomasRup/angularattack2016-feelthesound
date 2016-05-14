0b1fb0f587d2d7603d3bb0707dd8f1984a6524a3
tprupsys <tprupsys@gmail.com>
2016-05-14 18:04:24 +0300

    Some stuffy

--- app/childmode/childmode.component.ts
+++ app/childmode/childmode.component.ts
@@ -10,7 +10,7 @@ import { StreamService } from '../services/stream/stream.service';
     <div class="mode-container">
         <div class="header">Child mode</div>
         <div class="content">
-            <button [ngClass]="{streaming: streamingToggledOn, mute: !streamingToggledOn}" (click)="toggleStreaming()">Start streaming</button>
+            <button [ngClass]="{streaming: streamingToggledOn, mute: !streamingToggledOn}" (click)="toggleStreaming()">{{streamingButtonText}}</button>
         </div>
         <div class="footer"><a [routerLink]="['/modeselection']">Back</a></div>
     </div>
@@ -18,14 +18,17 @@ import { StreamService } from '../services/stream/stream.service';
 })
 export class ChildMode {
     private streamingToggledOn: boolean = false;
+    private streamingButtonText: string = 'Start streaming';
     
     constructor(private streamService: StreamService) {}
     
     toggleStreaming() {
         if (!this.streamingToggledOn) { 
-            this.streamService.start(); 
+            this.streamService.start();
+            this.streamingButtonText = 'Mute the stream';
         } else { 
-            this.streamService.stop(); 
+            this.streamService.stop();
+            this.streamingButtonText = 'Start streaming again';
         }
         
         this.streamingToggledOn = !this.streamingToggledOn;

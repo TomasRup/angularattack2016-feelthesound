c8b186905b565f061b7ed353b16f48bbde218d1a
tprupsys <tprupsys@gmail.com>
2016-05-14 15:20:59 +0300

    Added intro text, improved routing

--- app/modeselection/modeselection.component.ts
+++ app/modeselection/modeselection.component.ts
@@ -10,6 +10,10 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
       <a [routerLink]="['/parent']" class="selection-button">Parent Device</a>
       <a [routerLink]="['/singledevice']" class="selection-button">Single Device</a>
     </nav>
+    <div class="explanatory-text">
+        Feel the Sound helps parents stay closer to their sleeping children.<br>
+        The app monitors the sound around a device put near a baby and broadcasts it to another one. If a child starts crying, the parent device will flash and vibrate (this helps people with hearing impairments).
+    </div>
     `
 })
 export class ModeSelection {}

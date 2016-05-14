fd27d8e8a465e88bdb826d0decc68c2a3d8b90e0
tprupsys <tprupsys@gmail.com>
2016-05-14 20:12:46 +0300

    Added flags

--- app/childmode/childmode.component.ts
+++ app/childmode/childmode.component.ts
@@ -10,21 +10,23 @@ import { ChildStreamService } from '../services/stream/child-streamer.service';
     <div class="mode-container">
         <div class="header">Child mode</div>
         <div class="content">
+            <input [ngClass]="{disabled: entryDisabled}" type="text" placeholder="Enter Subscription ID" [disabled]="entryDisabled" [(value)]="subscriptionId">
             <button [ngClass]="{unsubscribed: service.getIsStarted(), subscribed: !service.getIsStarted()}"
                     (click)="toggleStreaming()">{{streamingButtonText}}</button>
-            <button>Disconnect all connected parents</button>
         </div>
         <div class="footer"><a [routerLink]="['/modeselection']">Back</a></div>
     </div>
     `
 })
 export class ChildMode {
+    private subscriptionId: string = '';
+    private entryDisabled: boolean = false;
     private streamingToggledOn: boolean = false;
     private streamingButtonText: string = 'Start capturing sounds';
     private toggleInProgress: boolean = false;
 
     constructor(private service: ChildStreamService) {
-        //TODO Auto toggle based on url
+        // TODO: Auto toggle based on url
     }
 
     toggleStreaming() {
@@ -33,7 +35,7 @@ export class ChildMode {
             this.service.stop();
             this.finishedToggle('Start capturing sounds')
         } else {
-            this.service.start("testStream", () => this.finishedToggle('Stop capturing sounds'));
+            this.service.start(this.subscriptionId, () => this.finishedToggle('Stop capturing sounds'));
         }
     }
 

1afa7a4901e9bd48606fa4a47a1795f379451f8d
Antanas Bastys <antanas.bastys@tieto.com>
2016-05-14 18:33:22 +0300

    Fixed compilation of media stream source

--- app/services/stream/stream.service.ts
+++ app/services/stream/stream.service.ts
@@ -6,32 +6,32 @@ export class StreamService {
     webSocket: WebSocket = undefined;
     currentStream = undefined;
     ended: Boolean = false;
-    
+
     start() {
        this.ended = false;
        this.webSocket = new WebSocket('ws://localhost:8080/hello');
        this.webSocket.binaryType = 'arraybuffer';
        this.webSocket.onopen = this.initWebSocket;
     }
-    
+
     stop() {
         if (!this.currentStream) return;
         this.currentStream.getTracks()[0].stop();
     }
-    
+
     private initWebSocket() {
         var n = <any> navigator;
         n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia || n.msGetUserMedia;
         n.getUserMedia({audio: true}, this.initRecorder, this.errorCallback);
     }
-    
+
     private initRecorder(stream) {
         this.currentStream = stream;
-        this.currentStream.addEventListener('ended', function() { 
+        this.currentStream.addEventListener('ended', function() {
             this.ended = true;
         });
 
-        let audioContext = new AudioContext();
+        let audioContext = <any> new AudioContext();
         let sampleRate = audioContext.sampleRate;
         var audioInput = audioContext.createMediaStreamSource(stream);
 
@@ -41,14 +41,14 @@ export class StreamService {
         audioInput.connect(recorder);
         recorder.connect(audioContext.destination);
     }
-    
+
     private recorderProcess(e) {
         if (this.ended) return;
         var left = e.inputBuffer.getChannelData(0);
         var data = left;
         this.webSocket.send(data);
     }
-    
+
     private errorCallback(error) {
         console.log(error);
     }

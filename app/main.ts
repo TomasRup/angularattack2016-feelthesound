35e8fc99eb4404ab91bcee6d7608afbfb4241325
Matas Ramoška <matas.ramoska@tieto.com>
2016-05-15 15:47:27 +0300

    Use HashLocationStrategy because of 404 errors from remote server on refresh

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,8 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { enableProdMode } from '@angular/core';
+import { provide } from '@angular/core';
 import { GlobalsService } from './services/common/globals.service';
 import { ApiService } from './services/common/api.service';
 import { ListenService } from './services/listen/listen.service';
@@ -9,6 +11,8 @@ import { AppComponent } from './app.component';
 enableProdMode();
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
+    provide(LocationStrategy,
+         {useClass: HashLocationStrategy}),
     GlobalsService,
     ListenService,
     ApiService]);

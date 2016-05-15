60b8abc681ad4b846ec6d656752a494cc68d0490
tprupsys <tprupsys@gmail.com>
2016-05-15 13:17:00 +0300

    Enabled prod mode

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,11 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router';
+import { enableProdMode } from '@angular/core';
 import { GlobalsService } from './services/common/globals.service';
 import { ListenService } from './services/listen/listen.service';
 import { AppComponent } from './app.component';
 
-
+enableProdMode();
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS, 
     GlobalsService,
